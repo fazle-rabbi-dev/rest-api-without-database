7c4bbe0f36cb902eb9883324434bb8afc964fea1
* <frabby983@gmail.com>
2022-08-22 15:14:21 +0600

    bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = require('./app');
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('Server is running at http://localhost:'+PORT);
